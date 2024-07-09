bbb66cde0ff293640a51f92a3690addf7f48f37c
Jomari Macaraig <jomari@auxahelth.com>
2024-07-09 18:46:38 +0800

    topic4: Packing and unpacking

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
-# add_number = lambda a, b: a + b
-# print(add_number(1,2))
-
-
-# cars = ["Toyota", "Ford", "Mitsubishi", "BMW", "Ferrari"]
-# print(cars)
+numbers = (1, 2, 3)
 #
-# cars.sort()
-# print(cars)
+# number1 = numbers[0]
+# number2 = numbers[1]
+# number3 = numbers[2]
 #
-# cars.sort(key=lambda a: len(a))
-# print(cars)
+# print(number1)
+# print(number2)
+# print(number3)
+#
+# number1, number2, number3 = numbers
+#
+# print(number1)
+# print(number2)
+# print(number3)
+#
+# print(numbers)
+# numbers = (number3, number2, number1)
+# print(numbers)
+#
+#
+# number1 = 1
+# number2 = 2
+#
+# temp = number1
+# number1 = number2
+# number2 = temp
+#
+# number1, number2 = number2, number1
+
+# test = "abc"
+# char1, char2, char3 = test
+# print(char1, char2, char3)
+
+# information = {
+#     "name": "Mary",
+#     "gender": "Female",
+#     "age": 25
+# }
+#
+# for key, value in information.items():
+#     print(f"{key} == {value}")
+
+# def add_items(*args):
+#     number1, number2, number3 = args
+#     return number1 + number2 + number3
+
+# print(add_items(1, 2, 3))
+# print(add_items(1, 2, 3, 4))
+# print(add_items(*numbers))
