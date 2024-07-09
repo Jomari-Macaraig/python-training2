ff4a96af1ed40dcfde0a43234b75fc112ff3d03c
Jomari Macaraig <jomari@auxahelth.com>
2024-07-09 17:10:48 +0800

    topic1: first function

---
+++ main.py
@@ -0,0 +1,9 @@
+# print("Hello World")
+# hello_world_length = len("Hello World")
+# print(hello_world_length)
+
+# def first_function():
+#     print("Hello World")
+#     print("Bye World")
+
+# first_function()
